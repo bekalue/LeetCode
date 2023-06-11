13d4d2ce8a3c2a3d296ad06f52a134dbb3b93a7a
Bekalu Geta <bekalu2054@gmail.com>
2023-06-11 15:04:34 +0300

    [feat]" `solve common prefix problem`

---
+++ Easy/0014-Longest_Common_Prefix/solution.py
@@ -0,0 +1,17 @@
+"""A module containing the solution
+"""
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        if not strs:
+            return ""
+        prefix = strs[0]
+        for i in range(1, len(strs)):
+            while strs[i].find(prefix) != 0:
+                prefix = prefix[:-1]
+                if not prefix:
+                    return ""
+        return prefix
